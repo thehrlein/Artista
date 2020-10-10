656c952734cabb7c4c460ef4a61f40a886cd672d
Tobias Hehrlein <tobias.hehrlein@appsfactory.de>
2020-10-10 16:53:41 +0200

    [FIX] crash as track not serializable

--- app/src/main/java/com/tobiapplications/artista/model/tracks/Track.kt
+++ app/src/main/java/com/tobiapplications/artista/model/tracks/Track.kt
@@ -1,7 +1,8 @@
 package com.tobiapplications.artista.model.tracks
 
 import com.google.gson.annotations.SerializedName
+import java.io.Serializable
 
 data class Track(@SerializedName("name") val name: String,
                  @SerializedName("url") val url: String,
-                 @SerializedName("duration") val duration: Int)
+                 @SerializedName("duration") val duration: Int): Serializable
